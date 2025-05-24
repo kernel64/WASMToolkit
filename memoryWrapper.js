ad3fc165962e0df8d023ddd0da31a7939f751d02
Mohamed Aymen <aymen.kernel@gmail.com>
2025-05-24 09:59:47 +0100

    adding float and 64bi support

--- memoryWrapper.js
+++ memoryWrapper.js
@@ -1,238 +1,190 @@
-class memoryWrapper {
-    ///*
-    //
-    // The goal of this class is to provide a way to easily handle the memory used by a wasm code
-    //
-    ///*
+///*
+//
+// The goal of this class is to provide a way to easily handle the memory used by a wasm code
+//
+///*
+
+class MemoryWrapper {
 
     constructor(memoryBuffer) {
-        ///*
-        // The constructor of the class. It needs a reference to the memory buffer
-        ///*
         this.memoryView = new DataView(memoryBuffer);
     }
 
+    // Reading methods
+
+    read64bitsUnsigned(address) {
+        return this.memoryView.getBigUint64(address, true);
+    }
+
+    read64bitsSigned(address) {
+        return this.memoryView.getBigInt64(address, true);
+    }
+
     read32bitsUnsigned(address) {
-        ///*
-        // Returns the 32 bits unsigned value in the memory buffer at the *address* memory address
-        ///*
-        return this.memoryView.getUint32(address);
+        return this.memoryView.getUint32(address, true);
     }
 
     read32bitsSigned(address) {
-        ///*
-        // Returns the 32 bits signed value in the memory buffer at the *address* memory address
-        ///*
-        return this.memoryView.getInt32(address);
+        return this.memoryView.getInt32(address, true);
     }
 
     read16bitsUnsigned(address) {
-        ///*
-        // Returns the 16 bits unsigned value in the memory buffer at the *address* memory address
-        ///*
-        return this.memoryView.getUint16(address);
+        return this.memoryView.getUint16(address, true);
     }
 
     read16bitsSigned(address) {
-        ///*
-        // Returns the 16 bits signed value in the memory buffer at the *address* memory address
-        ///*
-        return this.memoryView.getInt16(address);
+        return this.memoryView.getInt16(address, true);
     }
 
     read8bitsUnsigned(address) {
-        ///*
-        // Returns the 8 bits unsigned value in the memory buffer at the *address* memory address
-        ///*
         return this.memoryView.getUint8(address);
     }
 
     read8bitsSigned(address) {
-        ///*
-        // Returns the 8 bits signed value in the memory buffer at the *address* memory address
-        ///*
         return this.memoryView.getInt8(address);
     }
 
+    readFloat32(address) {
+        return this.memoryView.getFloat32(address, true);
+    }
+
+    readFloat64(address) {
+        return this.memoryView.getFloat64(address, true);
+    }
+
+    // Writing methods
+
+    write64bitsUnsigned(address, value) {
+        this.memoryView.setBigUint64(address, BigInt(value), true);
+    }
+
+    write64bitsSigned(address, value) {
+        this.memoryView.setBigInt64(address, BigInt(value), true);
+    }
+
     write32bitsUnsigned(address, value) {
-        ///*
-        // Write the 32 bits unsigned *value* value in the memory buffer at the *address* memory address
-        ///*
-        this.memoryView.setUint32(address, value);
+        this.memoryView.setUint32(address, value, true);
     }
 
     write32bitsSigned(address, value) {
-        ///*
-        // Write the 32 bits signed *value* value in the memory buffer at the *address* memory address
-        ///*
-        this.memoryView.setInt32(address, value);
+        this.memoryView.setInt32(address, value, true);
     }
 
     write16bitsUnsigned(address, value) {
-        ///*
-        // Write the 16 bits unsigned *value* value in the memory buffer at the *address* memory address
-        ///*
-        this.memoryView.setUint16(address, value);
+        this.memoryView.setUint16(address, value, true);
     }
 
     write16bitsSigned(address, value) {
-        ///*
-        // Write the 16 bits signed *value* value in the memory buffer at the *address* memory address
-        ///*
-        this.memoryView.setInt16(address, value);
+        this.memoryView.setInt16(address, value, true);
     }
 
     write8bitsUnsigned(address, value) {
-        ///*
-        // Write the 8 bits signed *value* value in the memory buffer at the *address* memory address
-        ///*
         this.memoryView.setUint8(address, value);
     }
 
     write8bitsSigned(address, value) {
-        ///*
-        // Write the 8 bits signed *value* value in the memory buffer at the *address* memory address
-        ///*
         this.memoryView.setInt8(address, value);
     }
 
+    writeFloat32(address, value) {
+        this.memoryView.setFloat32(address, value, true);
+    }
+
+    writeFloat64(address, value) {
+        this.memoryView.setFloat64(address, value, true);
+    }
+
+    // String and byte utilities
+
     readString(address) {
-        ///*
-        // Returns the string in the memory buffer starting at the *address* memory address.
-        // The function stop at the first '0x00' character encountered.
-        ///*
-        var result = "";
-        var i = 0;
-        var currentChar = this.read8bitsUnsigned(address);
+        let result = "";
+        let i = 0;
+        let currentChar = this.read8bitsUnsigned(address);
         while (currentChar !== 0) {
-            result = result + String.fromCharCode(currentChar);
-            i += 1;
+            result += String.fromCharCode(currentChar);
+            i++;
             currentChar = this.read8bitsUnsigned(address + i);
         }
         return result;
     }
 
     writeString(address, value) {
-        ///*
-        // Write the *value* string in the memory buffer starting at the *address* memory address.
-        // The function writes a '0x00' character at the end of the string.
-        ///*
-        var i = 0;
-        for (; i < value.length; i++){
+        for (let i = 0; i < value.length; i++) {
             this.write8bitsUnsigned(address + i, value.charCodeAt(i));
         }
-        this.write8bitsUnsigned(address + i, 0);
+        this.write8bitsUnsigned(address + value.length, 0);
     }
 
     readBytes(address, length) {
-        ///*
-        // Returns an array of values representing the bytes at the *address* memory address.
-        // The function reads exactly *length* bytes
-        ///*
-        var result = [];
-        for (var i = 0; i < length; i++) {
+        const result = [];
+        for (let i = 0; i < length; i++) {
             result.push(this.read8bitsUnsigned(address + i));
         }
         return result;
     }
 
     writeBytes(address, values) {
-        ///*
-        // Write the *values* bytes in the memory buffer starting at the *address* memory address.
-        // The *values* argument must be an array of bytes (int < 255).
-        ///*
-        for (var i = 0; i < values.length; i++) {
-            this.write8bitsUnsigned(address + i, values[i])
+        for (let i = 0; i < values.length; i++) {
+            this.write8bitsUnsigned(address + i, values[i]);
         }
+    }
+
+    // Search methods
+
+    searchValue(value, readFn, step) {
+        const addresses = [];
+        for (let i = 0; i <= this.memoryView.byteLength - step; i += step) {
+            if (readFn.call(this, i) === value) {
+                addresses.push(i);
+            }
+        }
+        return addresses;
+    }
+
+    search64bitsUnsigned(value) {
+        return this.searchValue(BigInt(value), this.read64bitsUnsigned, 8);
+    }
+
+    search64bitsSigned(value) {
+        return this.searchValue(BigInt(value), this.read64bitsSigned, 8);
     }
 
     search32bitsUnsigned(value) {
-        ///*
-        // Returns a list of memory addresses where the 32 bits unsigned value *value* is present in the memory buffer.
-        ///*
-        var addresses = [];
-        for (var i = 0; i < this.memoryView.byteLength; i += 4) {
-            if (this.read32bitsUnsigned(i) === value) {
-                addresses.push(i);
-            }
-        }
-        return addresses;
+        return this.searchValue(value, this.read32bitsUnsigned, 4);
     }
 
     search32bitsSigned(value) {
-        ///*
-        // Returns a list of memory addresses where the 32 bits signed value *value* is present in the memory buffer.
-        ///*
-        var addresses = [];
-        for (var i = 0; i < this.memoryView.byteLength; i += 4) {
-            if (this.read32bitsSigned(i) === value) {
-                addresses.push(i);
-            }
-        }
-        return addresses;
+        return this.searchValue(value, this.read32bitsSigned, 4);
     }
 
     search16bitsUnsigned(value) {
-        ///*
-        // Returns a list of memory addresses where the 16 bits unsigned value *value* is present in the memory buffer.
-        ///*
-        var addresses = [];
-        for (var i = 0; i < this.memoryView.byteLength; i += 2) {
-            if (this.read16bitsUnsigned(i) === value) {
-                addresses.push(i);
-            }
-        }
-        return addresses;
+        return this.searchValue(value, this.read16bitsUnsigned, 2);
     }
 
     search16bitsSigned(value) {
-        ///*
-        // Returns a list of memory addresses where the 16 bits signed value *value* is present in the memory buffer.
-        ///*
-        var addresses = [];
-        for (var i = 0; i < this.memoryView.byteLength; i += 2) {
-            if (this.read16bitsSigned(i) === value) {
-                addresses.push(i);
-            }
-        }
-        return addresses;
+        return this.searchValue(value, this.read16bitsSigned, 2);
     }
 
     search8bitsUnsigned(value) {
-        ///*
-        // Returns a list of memory addresses where the 8 bits unsigned value *value* is present in the memory buffer.
-        ///*
-        var addresses = [];
-        for (var i = 0; i < this.memoryView.byteLength; i += 1) {
-            if (this.read8bitsUnsigned(i) === value) {
-                addresses.push(i);
-            }
-        }
-        return addresses;
+        return this.searchValue(value, this.read8bitsUnsigned, 1);
     }
 
     search8bitsSigned(value) {
-        ///*
-        // Returns a list of memory addresses where the 8 bits signed value *value* is present in the memory buffer.
-        ///*
-        var addresses = [];
-        for (var i = 0; i < this.memoryView.byteLength; i += 1) {
-            if (this.read8bitsSigned(i) === value) {
-                addresses.push(i);
-            }
-        }
-        return addresses;
+        return this.searchValue(value, this.read8bitsSigned, 1);
+    }
+
+    searchFloat32(value) {
+        return this.searchValue(value, this.readFloat32, 4);
+    }
+
+    searchFloat64(value) {
+        return this.searchValue(value, this.readFloat64, 8);
     }
 
     searchString(value) {
-        ///*
-        // Returns a list of memory addresses where the string *value* is present in the memory buffer.
-        // The returned values point to the start of the strings.
-        // The function considers a string is finished at the first '0x00' character encountered.
-        ///*
-        var potAddresses = this.search8bitsUnsigned(value.charCodeAt(0));
-        var addresses = [];
-        for (var address of potAddresses) {
+        const potAddresses = this.search8bitsUnsigned(value.charCodeAt(0));
+        const addresses = [];
+        for (let address of potAddresses) {
             if (this.readString(address) === value) {
                 addresses.push(address);
             }
@@ -241,32 +193,26 @@ class memoryWrapper {
     }
 
     searchStringRegex(value) {
-        ///*
-        // Returns a list of memory addresses where the regex *value* matches in the memory buffer.
-        // The returned values point to the start of the strings.
-        // The function considers a string is finished at the first '0x00' character encountered.
-        //
-        // Not as slow as I thought it would be...
-        ///*
         try {
             value = new RegExp(value, 'g');
-        } catch(e) {
-            console.log("Invalid regular expression")
-            return []
+        } catch (e) {
+            console.log("Invalid regular expression");
+            return [];
         }
-        var addresses = [];
-        var currentAddress = 0;
+
+        const addresses = [];
+        let currentAddress = 0;
 
         while (currentAddress < this.memoryView.byteLength) {
             if (this.read8bitsUnsigned(currentAddress) === 0) {
-                currentAddress += 1;
+                currentAddress++;
             } else {
-                var tmpString = this.readString(currentAddress);
-                let found;
-                while ((found = value.exec(tmpString)) !== null) {
-                    addresses.push(currentAddress + found.index)
+                const tmpString = this.readString(currentAddress);
+                let match;
+                while ((match = value.exec(tmpString)) !== null) {
+                    addresses.push(currentAddress + match.index);
                 }
-                currentAddress += tmpString.length;
+                currentAddress += tmpString.length + 1;
             }
         }
 
